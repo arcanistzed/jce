688de9c78632cd4890332b1811107ae243cdbc3a
arcanist <arcanistzed@gmail.com>
2021-08-19 00:48:08 -0400

    feat: reimplement activating editors

--- scripts/module.js
+++ scripts/module.js
@@ -1,27 +1,18 @@
-let sourceContent;
-
-// Create Jce editor Form Application
+/** Manage JCE Journal Sheet */
 class Jce extends JournalSheet {
 	/** The module's ID */
 	static ID = "jce";
+
+	/** Compatible editor libraries */
+	static EDITORS = ["acelib", "_CodeMirror"]
 
 	/** @override */
 	static get defaultOptions() {
 		return mergeObject(super.defaultOptions, {
 			classes: ["jce-sheet", "sheet"],
 			template: `modules/jce/templates/sheet.hbs`,
-			id: 'journal-code-editor',
-			width: window.innerWidth * 3 / 4,
-			height: window.innerHeight * 3 / 4
+			id: 'journal-code-editor'
 		});
-	};
-
-	/** @override */
-	activateListeners(html) {
-		super.activateListeners(html);
-
-		// Everything below here is only needed if the sheet is editable
-		if (!this.options.editable) return;
 	};
 
 	/** @override */
@@ -33,145 +24,130 @@ class Jce extends JournalSheet {
 	getData() {
 		// Retrieve the data structure from the base sheet
 		const context = super.getData();
-		console.log(context)
 
-		context.editors = ["acelib", "_CodeMirror"];
-
+		// Add editor list to context
+		context.editors = Jce.EDITORS;
 		return context;
 	};
 
-	// when saved, update journal entry with new data
-	/* async _updateObject() {
+	/** @override */
+	activateListeners(html) {
+		super.activateListeners(html);
 
-		var editor;
-		var useCodeMirror;
+		// Everything below here is only needed if the sheet is editable
+		if (!this.options.editable) return;
 
-		// Check if user is using CodeMirror or Ace
-		if (game.modules.get("_CodeMirror")?.active) {
-			useCodeMirror = game.settings.get(Jce.ID, "CodeMirror");
+		// Get the editor choice select box
+		let selectBox = html[0].querySelector("#jce-select-editor");
+
+		// Activate the currently selected editor
+		this.activateEditor(selectBox.value, html[0]);
+
+		// Listen for whenever the select box changes
+		selectBox.addEventListener("change", () => {
+			// Activate the corresponding editor
+			this.activateEditor(selectBox.value, html[0]);
+
+			// Hide select box
+			html[0].querySelector("#jce-sheet-header").style.display = "none";
+		});
+
+		// Save on submit
+		html[0].querySelector("#jce-save").addEventListener("click", this._updateObject);
+	};
+
+	/** Activates a specific editor
+	 * @param {String} editorName - The name of the editor to activate
+	 * @param {HTMLElement} html - The element at the root of the sheet
+	*/
+	activateEditor(editorName, html) {
+		// Alert and exit if module not enabled
+		if (!game.modules.get(editorName)?.active) {
+			ui.notifications.error("JCE | You must enable " + editorName);
+			return;
 		};
-		if (useCodeMirror === true) {
-			editor = document.getElementsByClassName("CodeMirror")[0].CodeMirror;
-		} else {
+
+		// Get current Jounral Entry data
+		let sourceId = this.object.id;
+		let sourceContent = this.object.data.content;
+
+		// Enable selected editor
+		let editor;
+		if (editorName === "acelib") {
+
+			// Initialise ace editor
 			editor = ace.edit("jce-editor");
+
+			// Set ace options
+			editor.setOptions(ace.userSettings);
+
+			// Set to html mode
+			editor.session.setMode("ace/mode/html");
+
+			// Populate with journal entry source code
+			editor.setValue(sourceContent);
+
+			// Show keyboard shortcuts
+			editor.commands.addCommand({
+				name: "showKeyboardShortcuts",
+				bindKey: { win: "Ctrl-Alt-h", mac: "Command-Alt-h" },
+				exec: editor => {
+					ace.config.loadModule("ace/ext/keybinding_menu", module => {
+						module.init(editor);
+						editor.showKeyboardShortcuts();
+					})
+				}
+			});
+		} else if (editorName === "_CodeMirror") {
+			// Replace Div with Textarea
+			const textarea = document.createElement("textarea");
+			html.querySelector("#jce-editor").replaceWith(textarea);
+			textarea.id = "jce-editor";
+
+			// Initialise Code Mirror
+			editor = CodeMirror.fromTextArea(textarea, {
+				mode: "html",
+				...CodeMirror.userSettings,
+				lineNumbers: true,
+				autofocus: true
+			});
+
+			// Set initial value
+			editor.setValue(sourceContent);
 		};
-		var output = editor.getValue();
-		var data = {
+	};
+
+	// When saved, update the Journal Entry with new data
+	async _updateObject() {
+
+		// Get current editor
+		let editor, editorName = document.querySelector("#jce-select-editor").value;
+		if (editorName === "acelib") {
+			editor = ace.edit("jce-editor");
+		} else if (editorName === "_CodeMirror") {
+			editor = document.querySelector("#jce-editor + .CodeMirror");
+		};
+
+		// Create update package
+		const output = editor.getValue();
+		const data = {
 			_id: this.sourceId,
 			content: output
 		};
 
-		// Only update if changes have been made
-		if (this.sourceContent != output) JournalEntry.update([data]);
-	}; */
+		// Update if changes have been made
+		if (this.sourceContent !== output) JournalEntry.updateDocuments([data]);
+		this.close();
+	};
 };
 
-/* Hooks.on("renderJournalSheet", app => {
-	let AutoOpen = game.settings.get(Jce.ID, "AutoOpen");
-	let sourceContent = app.object.data.content.trim();
-	let sourceId = app.object.data._id;
-
-	const open = () => {
-		new Jce(sourceContent, sourceId).render(true);
-	};
-
-	// If the user has enabled it, when a journal sheet is opened, open the JCE right away
-	if (AutoOpen === true && game.user.isGM) {
-		open();
-	};
-
-	// Open when they click on edit
-	document.querySelector(".editor-edit").onclick = open;
-}); */
-
-// Add context menu option for opening the jce editor
-/* Hooks.on('getJournalDirectoryEntryContext', (_html, contextEntries) => {
-	if (game.user.isGM) { // Only show for GMs
-		contextEntries.push({
-			name: game.i18n.localize("jce.ContextMenu"),
-			icon: `<i class="fas fa-code"></i>`,
-			condition: {},
-			callback: data => {
-				let sourceId = data[0].dataset.entityId;
-				let sourceJournal = game.journal.get(sourceId);
-				if (sourceJournal.data.content != null) { var sourceContent = sourceJournal.data.content.trim() }; // only trim content if content exists
-
-				new Jce(sourceContent, sourceId).render(true); // render jce editor
-			}
-		});
-	};
-}); */
-
-/* Hooks.on("renderJce", app => {
-
-	var editor;
-	var useCodeMirror;
-
-	// Check if user wants to use CodeMirror or Ace
-	if (game.modules.get("_CodeMirror")?.active) {
-		useCodeMirror = game.settings.get(Jce.ID, "CodeMirror");
-	};
-	if (useCodeMirror === true) {
-
-		// Initialise Code Mirror
-		var ce = document.getElementById("jce-editor");
-		var editor = CodeMirror(node => ce.parentNode.replaceChild(node, ce), {
-			value: app.sourceContent,
-			mode: "html",
-			...CodeMirror.userSettings,
-			lineNumbers: true,
-			inputStyle: "contenteditable",
-			autofocus: true
-		});
-		editor.setSize(null, '100%');
-
-	} else {
-		// initialise ace editor
-		editor = ace.edit("jce-editor");
-
-		// set ace options
-		editor.setOptions(ace.userSettings);
-
-		// set to html mode
-		editor.session.setMode("ace/mode/html");
-
-		// populate with journal entry source code
-		editor.setValue(app.sourceContent);
-
-		// show keyboard shortcuts
-		editor.commands.addCommand({
-			name: "showKeyboardShortcuts",
-			bindKey: { win: "Ctrl-Alt-h", mac: "Command-Alt-h" },
-			exec: editor => {
-				ace.config.loadModule("ace/ext/keybinding_menu", module => {
-					module.init(editor);
-					editor.showKeyboardShortcuts()
-				})
-			}
-		});
-
-		// suppress DOCTYPE warning
-		var session = editor.getSession();
-		session.on("changeAnnotation", () => {
-			var annotations = session.getAnnotations() || [], i = len = annotations.length;
-			while (i--) {
-				if (/doctype first\. Expected/.test(annotations[i].text)) {
-					annotations.splice(i, 1);
-				};
-			};
-			if (len > annotations.length) {
-				session.setAnnotations(annotations);
-			};
-		});
-	};
-}); */
-
+// Register new Journal Entry sheets
 Hooks.on("preDocumentSheetRegistrarInit", (settings) => {
 	settings["JournalEntry"] = true;
 });
 
-Hooks.on("documentSheetRegistrarInit", (documentTypes) => {
-	console.log(documentTypes)
+// Register JCE sheet
+Hooks.on("documentSheetRegistrarInit", () => {
 	Journal.registerSheet?.(Jce.ID, Jce, {
 		types: ["base"],
 		makeDefault: true,
